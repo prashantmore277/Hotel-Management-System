3a7ecf19ad97b7c0c989b3df55787d93503de142
prashantmore277 <prashantmore277@gmail.com>
2022-04-28 09:24:19 +0530

    Creates Login UI And Its Implementation & Creates Jframes : -AdminHome -Home

--- src/project/ConnectionProvider.java
+++ src/project/ConnectionProvider.java
@@ -15,8 +15,6 @@ public class ConnectionProvider {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con =  DriverManager.getConnection("jdbc:mysql://localhost:3307/hotel","root","1234");
-            JOptionPane.showMessageDialog(null,"Table Created Successfully");
-
             return con;
             
         }
